957b34eea6bfd3ce8ddc646b9a855bf2cba59265
Leonardo Lima <leonardo.b.lima1@gmail.com>
2023-11-25 19:10:29 -0300

    criacao da classe de tratamento de errors

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Dev!');
+import express from 'express';
+import cors from 'cors';
+import { routes } from './routes';
+
+const port = 3333;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => console.log(`Server start on port:${port}`));
