cf2211d56708102ea0b8c13041809e02c67e540d
Leonardo Lima <leonardo.b.lima1@gmail.com>
2023-11-25 19:18:35 -0300

    criacao do midle de error

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { routes } from './routes';
+import { AppError } from '@shared/errors/AppError';
 
 const port = 3333;
 const app = express();
@@ -8,5 +9,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message });
+  }
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(port, () => console.log(`Server start on port:${port}`));
