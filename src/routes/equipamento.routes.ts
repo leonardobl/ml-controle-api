00b5efe9cb10f5e4ecc0202baca3ccbd8ff75dcd
Leonardo Lima <leonardo.b.lima1@gmail.com>
2023-11-27 21:10:47 -0300

    remocao das tipagem de retorno das fu ncoes

--- src/routes/equipamento.routes.ts
+++ src/routes/equipamento.routes.ts
@@ -1,24 +1,17 @@
-import { Response, Router } from 'express';
+import { Router } from 'express';
 import { EquipamentoController } from '../controllers/EquipamentoController';
-import { IEquipamentoDto } from '../types/equipamento';
 
 export const routesEquipamentos = Router();
 
 const eqController = new EquipamentoController();
 
-routesEquipamentos.get(
-  '/equipamento',
-  async (req, res): Promise<Response<IEquipamentoDto[]>> => {
-    return await eqController.list(req, res);
-  },
-);
+routesEquipamentos.get('/equipamento', async (req, res) => {
+  return await eqController.list(req, res);
+});
 
-routesEquipamentos.post(
-  '/equipamento',
-  async (req, res): Promise<Response<IEquipamentoDto>> => {
-    return await eqController.create(req, res);
-  },
-);
+routesEquipamentos.post('/equipamento', async (req, res) => {
+  return await eqController.create(req, res);
+});
 
 routesEquipamentos.get('/equipamento/:id', async (req, res) => {
   return await eqController.findById(req, res);
