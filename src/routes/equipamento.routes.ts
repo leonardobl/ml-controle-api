23f6a3388b2b87f82652bfc10947e75b20d4c8ce
Leonardo Lima <leonardo.b.lima1@gmail.com>
2023-11-30 07:20:23 -0300

    instalacao da dev dependece faker

--- src/routes/equipamento.routes.ts
+++ src/routes/equipamento.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { EquipamentoController } from '../controllers/EquipamentoController';
+import { faker } from '@faker-js/faker';
 
 export const routesEquipamentos = Router();
 
